c6b8d7e5ee4dfde59f17e9cabe2e257dd834458e
notlikethiss <notlikeethiss@gmail.com>
2024-03-26 21:14:55 +0300

    setting up a project

---
+++ components/FilmCard.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+const FilmCard: FC = () => {
+  return <div>FilmCard</div>;
+};
+
+export default FilmCard;
---
+++ components/MediaPlayer.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+const MediaPlayer: FC = () => {
+  return <div>player</div>;
+};
+
+export default MediaPlayer;
